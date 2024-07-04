feeed6c78f6fc7d4b883529043b5214555266a8c
Jefferson Frade <jefferrson.frade@gmail.com>
2024-07-04 19:23:39 -0300

    Adicionado autenticacao

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,7 +9,9 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__.'/../routes/api.php',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->use([
+            \App\Http\Middleware\AlwaysExpectsJson::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
